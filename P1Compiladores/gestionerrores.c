72e093d77a4ad5f1e4fcf6fa097e9723744a59bb
Alextryfg <alex.fuentes@rai.usc.es>
2023-03-09 14:55:28 +0100

    a

---
+++ P1Compiladores/gestionerrores.c
@@ -0,0 +1,32 @@
+
+#include "gestionerrores.h"
+#include <stdio.h>
+
+//File erros
+void errorArchivo(int codigo){
+
+	switch(codigo){
+	case 1:
+		/*File cannot be opened*/
+		printf("No se puede abrir el archivo\n");
+		break;
+	case 2:
+		/*Error reading file*/
+		printf("Error en la lectura dle archivo\n");
+		break;
+	}
+}
+
+//Lexeme erros
+void errorLexico(int codigo, char *lexema) {
+	switch (codigo) {
+	case 1:
+		/*Lexeme too large*/
+		printf("El lexema sobrepasa el tamaño máximo\n");
+		break;
+	case 2:
+		/*Misformed lexema*/
+		printf("Lexema '%s' mal formado\n", lexema);
+		break;
+	}
+}
---
+++ P1Compiladores/gestionerrores.h
@@ -0,0 +1,7 @@
+#ifndef XESTIONERROS_XESTIONERROS_H_
+
+
+void errorArchivo(int codigo);
+void errorLexico(int codigo, char *lexema);
+
+#endif /* XESTIONERROS_XESTIONERROS_H_ */
