9e126c55ea390f83ea638da49913b9e629cc9993
Alextryfg <alex.fuentes@rai.usc.es>
2023-03-10 13:59:00 +0100

    Modificacion y adaptacion de codigo a compilador D

--- P1Compiladores/definiciones.h
+++ P1Compiladores/definiciones.h
@@ -1,8 +1,9 @@
 #ifndef DEFINICIONS_H_
 #define DEFINITIONS_H_
 
+//Keywords inciales en la tabla de simbolos
 #define IMPORT 300
-#define ID 301
+#define ID 301  //STD, STDIO, son identificadores
 #define STRING 302
 #define INT 303
 #define FLOAT 304
@@ -10,6 +11,18 @@
 #define FOREACH 306
 #define RETURN 307
 #define VOID 308
+
+//Operadores
+#define OPARITMETICO 400
+#define OPRELACIONAL 401
+#define OPASIGNACION 402
+#define OPINCREMENTO 403
+#define OPLOGICO 404
+#define OPUNARIO 405
+
+//Los delimitadores, tales como corchete, parentesis, llaves, puntos, comas, Asterisco (), [] , {} , . , , , * , + , /, ; y asi,
+//simplemente devolvere su código ASCII. De esta manera, no tendre que crear
+//un codigo para ellos en la tabla de símbolos.
 
 #endif /* DEFINITIONS_H_ */
 
