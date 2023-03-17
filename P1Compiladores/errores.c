0d208920594d878cca26edebd03f0a8ef47db3bb
Alextryfg <alex.fuentes@rai.usc.es>
2023-03-17 17:57:22 +0100

    done

--- P1Compiladores/errores.c
+++ P1Compiladores/errores.c
@@ -7,20 +7,20 @@
 void errorD(int idCodigo){
     switch(idCodigo){
         case 1:
-            perror("\nError: No se pudo abrir el archivo\n");
+            perror("\nNo se pudo abrir el archivo\n");
             break;
         case 2:
-            perror("\nError: El tamaño del lexema es superior al limite\n");
+            perror("\nEl tamaño del lexema es superior al limite\n");
             break;
         case 3:
-            perror("\nError: No se puede cargar el bloque\n");
+            perror("\nNo se puede cargar el bloque\n");
             break;
         case 4:
             perror("\nLexema mal formado");
         case 5:
             perror("\nParametros insuficiente en linea de comandos ./compiladorD regression.d\n");
         default:
-            perror("\nError: No se pudo identificar el error");
+            perror("\nNo se pudo identificar el error");
             break;
 
     }
